9f7e78c6120715b9e3e8e4014471970219b4e15c
윤희수 <hsyoon@iparking.co.kr>
2019-12-26 15:41:12 +0900

    cypress 설치

---
+++ cypress/integration/signIn.spec.ts
@@ -0,0 +1,3 @@
+describe('로그인 페이지 테스트 코드', () => {
+  cy.visit('http://localhost:4200')
+})
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-test-code';
+  public signInForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit() {
+    this.signInForm = new FormGroup({
+      id: new FormControl(''), pw: new FormControl('')
+    });
+  }
+
+  login(){
+    if (!this.signInForm.controls.id.value || !this.signInForm.controls.pw.value) {
+      alert('필수값이 누락되었습니다.');
+      return;
+    }
+
+    alert('로그인 되었습니다.')
+  }
 }
